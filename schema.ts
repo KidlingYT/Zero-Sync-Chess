5165dae2f061e792dd43486608e2dda389efe91b
Elijah Symons <130009409+KidlingYT@users.noreply.github.com>
2025-07-06 01:07:14 -0400

    feat: add game table

--- schema.ts
+++ schema.ts
@@ -5,13 +5,15 @@ import {
     definePermissions,
     ANYONE_CAN_DO_ANYTHING,
     PermissionsConfig,
+    number,
+    boolean,
 } from "@rocicorp/zero";
 
 type Authdata = {
     userID: string;
 };
 
-const zChessUser = table("zChessUser")
+const z_chess_user = table("z_chess_user")
     .columns({ id: string(), name: string() })
     .primaryKey("id");
 
@@ -19,12 +21,25 @@ const users = table("users")
     .columns({ name: string(), email: string(), password: string() })
     .primaryKey("email");
 
-export const schema = createSchema({ tables: [zChessUser, users] });
+const chess_games = table("chess_games")
+    .columns({
+        id: number(),
+        whitePlayerName: string(),
+        blackPlayerName: string(),
+        fen: string(),
+        is_active: boolean(),
+    })
+    .primaryKey("id");
+
+export const schema = createSchema({
+    tables: [z_chess_user, users, chess_games],
+});
 export type Schema = typeof schema;
 
 export const permissions = definePermissions<Authdata, Schema>(schema, () => {
     return {
-        zChessUser: ANYONE_CAN_DO_ANYTHING,
+        z_chess_user: ANYONE_CAN_DO_ANYTHING,
         users: ANYONE_CAN_DO_ANYTHING,
+        chess_games: ANYONE_CAN_DO_ANYTHING,
     } satisfies PermissionsConfig<Authdata, Schema>;
 });
