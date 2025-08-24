6140ed27e5dbdd1c77117b302ec521e17e16e490
Elijah Symons <130009409+KidlingYT@users.noreply.github.com>
2025-08-24 08:23:30 -0400

    docs: add note

--- src/resourceAccess/supabaseAccess.ts
+++ src/resourceAccess/supabaseAccess.ts
@@ -1,2 +1,3 @@
 //  Resource Access encapsulates resource access volatility.
 // This resource access file provides access to supabase.
+// Note that supabase access could use the zero resource or eventually another resource to access supabase.
