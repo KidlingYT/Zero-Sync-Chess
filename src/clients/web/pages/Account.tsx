aaac0285570a3ec14a001d8c74ce4fba1c4cabf8
Elijah Symons <130009409+KidlingYT@users.noreply.github.com>
2025-09-22 10:12:54 -0400

    chore: reset account page

--- src/clients/web/pages/Account.tsx
+++ src/clients/web/pages/Account.tsx
@@ -1,37 +1,9 @@
 import Header from "@/components/Header";
-import { Input } from "@/components/ui/input";
-import { useZero } from "@rocicorp/zero/react";
-import { useQuery } from "@rocicorp/zero/react";
 
 const AccountPage = () => {
-    const z = useZero();
-
-    const [user] = useQuery(z.query.users);
-
-    function UpdateDBName(value: string) {
-        z.mutate.users.update({ email: user[0].email, name: value });
-    }
     return (
         <main>
             <Header />
-            <div>
-                <h2 className="text-3xl">
-                    You are currently logged in as {z.userID}
-                </h2>
-                <h2 className="text-3xl">
-                    You are currently logged in as {user[0].email}
-                </h2>
-                <h2 className="text-3xl">
-                    You are currently logged in as {user[0].name}
-                </h2>
-                <h2 className="text-3xl">
-                    You are currently logged in as {user[0].password}
-                </h2>
-                <Input
-                    onChange={(e) => UpdateDBName(e.target.value)}
-                    value={user[0].name}
-                />
-            </div>
         </main>
     );
 };
