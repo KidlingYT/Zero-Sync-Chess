f3ba39975067d5f1f4851bf464d5d3e67bbea25a
Elijah Symons <130009409+KidlingYT@users.noreply.github.com>
2025-09-22 10:12:40 -0400

    feat: initialize matchmaking manager

--- src/utilities/security.ts
+++ src/utilities/security.ts
@@ -1,1 +1,55 @@
 // Security Utility encapsulates authentication volatility.
+import { createClient } from "@supabase/supabase-js";
+import { create } from "zustand";
+
+const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+export class SecurityUtility {
+    constructor() {}
+
+    async loginWithEmailAndPassword(email: string, password: string) {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+        if (error) throw error;
+        return data.user;
+    }
+
+    async signUpNewUser(email: string, password: string, redirectUrl?: string) {
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+            options: { emailRedirectTo: redirectUrl },
+        });
+        if (error) throw error;
+        return data.user;
+    }
+
+    async getSession() {
+        return supabase.auth.getSession();
+    }
+
+    logout() {
+        return supabase.auth.signOut();
+    }
+
+    async isLoggedIn() {
+        return supabase.auth
+            .getSession()
+            .then((session) => session.data.session !== null);
+    }
+}
+
+interface SecurityUtilityStore {
+    securityUtility: SecurityUtility | undefined;
+    setSecurityUtility: (securityUtility: SecurityUtility | undefined) => void;
+}
+
+export const useSecurityUtilityStore = create<SecurityUtilityStore>((set) => ({
+    securityUtility: undefined,
+    setSecurityUtility: (securityUtility) => set({ securityUtility }),
+}));
