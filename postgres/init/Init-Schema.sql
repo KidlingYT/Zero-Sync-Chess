06f75eb7c43aa29bd862e9d2e462b04acb76c1e3
Elijah Symons <130009409+KidlingYT@users.noreply.github.com>
2025-12-14 19:22:54 -0500

    feat: update init sql

--- postgres/init/Init-Schema.sql
+++ postgres/init/Init-Schema.sql
@@ -1,18 +1,13 @@
-CREATE TABLE chess_games {
-    id INT IDENTITY(1,1) PRIMARY KEY
-    whitePlayerName NVARCHAR NOT NULL
-    blackPlayerName NVARCHAR NOT NULL
-    fen NVARCHAR NOT NULL
+CREATE TABLE chess_games (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    white_player_name TEXT NOT NULL,
+    black_player_name TEXT NOT NULL,
+    fen TEXT NOT NULL,
     is_active BOOLEAN NOT NULL
-}
+);
 
-CREATE TABLE z_chess_user {
-    id INT IDENTITY(1,1) PRIMARY KEY
-    name NVARCHAR NOT NULL
-}
-
-CREATE TABLE users {
-    name NVARCHAR NOT NULL
-    email NVARCHAR PRIMARY KEY
-    password NVARCHAR NOT NULL
-}
+CREATE TABLE users (
+    name TEXT NOT NULL,
+    email TEXT PRIMARY KEY,
+    password TEXT NOT NULL
+);
