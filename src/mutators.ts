a670e2b7a17328226899bebbe513c1c28cb7d42f
Elijah Symons <130009409+KidlingYT@users.noreply.github.com>
2025-12-15 20:44:36 -0500

    feat: 7.	go to schema.ts and update the builder and export zql. Update mutators and queries

---
+++ src/mutators.ts
@@ -0,0 +1,57 @@
+﻿import { defineMutator, defineMutators } from "@rocicorp/zero";
+import { z } from "zod";
+
+const chessGameSchema = z.object({
+    id: z.string(),
+    white_player_name: z.string(),
+    black_player_name: z.string(),
+    fen: z.string(),
+    is_active: z.boolean(),
+    white_time: z.number(),
+    black_time: z.number(),
+});
+
+const usersSchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+});
+
+export const mutators = defineMutators({
+    chess_games: {
+        create: defineMutator(chessGameSchema, async ({ tx, args }) => {
+            await tx.mutate.chess_games.insert(args);
+        }),
+        update: defineMutator(
+            { ...z.object({ id: z.string() }), ...chessGameSchema.partial() },
+            async ({ tx, args }) => {
+                await tx.mutate.chess_games.update(args);
+            }
+        ),
+        delete: defineMutator(
+            z.object({ id: z.string() }),
+            async ({ tx, args }) => {
+                await tx.mutate.chess_games.delete(args);
+            }
+        ),
+    },
+    users: {
+        users: {
+            create: defineMutator(usersSchema, async ({ tx, args }) => {
+                await tx.mutate.users.insert(args);
+            }),
+            update: defineMutator(
+                { ...z.object({ id: z.string() }), ...usersSchema.partial() },
+                async ({ tx, args }) => {
+                    await tx.mutate.users.update(args);
+                }
+            ),
+            delete: defineMutator(
+                z.object({ id: z.string() }),
+                async ({ tx, args }) => {
+                    await tx.mutate.users.delete(args);
+                }
+            ),
+        },
+    },
+});
---
+++ src/queries.ts
@@ -0,0 +1,12 @@
+﻿import { defineQueries, defineQuery } from "@rocicorp/zero";
+import { zql } from "../schema";
+import z from "zod";
+
+export const queries = defineQueries({
+    chess_games: {
+        all: defineQuery(() => zql.chess_games.orderBy("id", "desc")),
+        one: defineQuery(z.object({ id: z.string() }), ({ args: { id } }) =>
+            zql.chess_games.where("id", id).one()
+        ),
+    },
+});
